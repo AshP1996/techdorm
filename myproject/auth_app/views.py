4b37ce48e3d34fa708193f599e15f13cc1cd73d6
Ashish <ashish01prajapati01@gmail.com>
2024-05-22 12:34:37 +0530

    update views.py

--- myproject/auth_app/views.py
+++ myproject/auth_app/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserSerializer
-from .token import CustomTokenObtainPairSerializer
+from .models import CustomUser
 
 @api_view(['POST'])
 def register(request):
@@ -24,9 +24,14 @@ def login(request):
     password = request.data.get('password')
     user = authenticate(username=username, password=password)
     if user is not None:
-        refresh = CustomTokenObtainPairSerializer.get_token(user)
+        refresh = RefreshToken.for_user(user)
         return Response({
             'refresh': str(refresh),
             'access': str(refresh.access_token),
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email,
+            'is_active': user.is_active,
+            'role': user.role,
         })
     return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
